7c7d1dce814cd15eb89512397094bc34579569b9
Yacob Madiana <yacob.madiana@gmail.com>
2020-01-16 16:02:45 +0700

    First Commit

---
+++ src/views/Registration.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Segment, Header } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+import './Registration.css';
+
+function Registration() {
+  return (
+    <div>
+      <Segment><Header as='h2'>Registration</Header></Segment>
+    </div>
+  );
+}
+
+export default Registration;
