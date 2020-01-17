7cc5127070c88163c22a2cd1d9e1ee13902ecdba
Yacob Madiana <yacob.madiana@gmail.com>
2020-01-17 14:33:20 +0700

    Phone, first name, last name, email validation

--- src/views/Registration.js
+++ src/views/Registration.js
@@ -6,7 +6,8 @@ import {
   februaryDateOptions,
   februaryKabisatDateOptions,
   monthsOptions,
-  yearOptions
+  yearOptions,
+  phoneValidator
 } from "../utils/Options";
 import "semantic-ui-css/semantic.min.css";
 import "./Registration.css";
@@ -22,6 +23,12 @@ function Registration() {
   const [year, setYear] = useState("1970");
   const [email, setEmail] = useState("");
 
+  //Error Message default is 'false' --> No Error Message
+  const [errorPhoneNumber, setErrorPhoneNumber] = useState(false);
+  const [errorFirstName, setErrorFirstName] = useState(false);
+  const [errorLastName, setErrorLastName] = useState(false);
+  const [errorEmail, setErrorEmail] = useState(false);
+
   // Submitted Value
   const [submittedPhoneNumber, setSubmittedPhoneNumber] = useState("");
   const [submittedFirstName, setSubmittedFirstName] = useState("");
@@ -36,6 +43,15 @@ function Registration() {
     switch (name) {
       case "phoneNumber":
         setPhoneNumber(value);
+        /* let phonePrefix = value.substring(0, 4);
+        if (phoneValidator.includes(phonePrefix)) {
+          setErrorPhoneNumber(false);
+        } else {
+          setErrorPhoneNumber({
+            content: "Please enter a valid Indonesian phone number",
+            pointing: "below"
+          });
+        } */
         return;
       case "firstName":
         setFirstName(value);
@@ -63,16 +79,73 @@ function Registration() {
     }
   };
 
+  const handleSubmit = () => {
+    // Phone Number Validator
+    let phonePrefix = phoneNumber.substring(0, 4);
+    if (phoneNumber === "") {
+      setErrorPhoneNumber({
+        content: "Phone number is required",
+        pointing: "below"
+      });
+    } else {
+      if (phoneValidator.includes(phonePrefix)) {
+        setErrorPhoneNumber(false);
+      } else {
+        setErrorPhoneNumber({
+          content: "Please enter a valid Indonesian phone number",
+          pointing: "below"
+        });
+      }
+    }
+
+    // First Name Validator
+    if (firstName === "") {
+      setErrorFirstName({
+        content: "First name is required",
+        pointing: "below"
+      });
+    } else {
+      setErrorFirstName(false);
+    }
+
+    // Last Name Validator
+    if (lastName === "") {
+      setErrorLastName({
+        content: "Last name is required",
+        pointing: "below"
+      });
+    } else {
+      setErrorLastName(false);
+    }
+
+    // Email Validator
+    if (email === "") {
+      setErrorEmail({
+        content: "Email is required",
+        pointing: "below"
+      });
+    } else {
+      let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      let validEmail = re.test(String(email).toLowerCase());
+      console.log("Valid email = ", validEmail);
+      if (validEmail === false) {
+        setErrorEmail({
+          content: "Please enter a valid email address",
+          pointing: "below"
+        });
+      } else {
+        setErrorEmail(false);
+      }
+    }
+  };
+
   return (
     <div>
       <Segment>
         <Header as="h2">Registration</Header>
         <Form>
           <Form.Input
-            error={{
-              content: "Please enter a valid Indonesian phone number",
-              pointing: "below"
-            }}
+            error={errorPhoneNumber}
             fluid
             required
             placeholder="Mobile Number"
@@ -81,10 +154,7 @@ function Registration() {
             onChange={handleChange}
           />
           <Form.Input
-            error={{
-              content: "Please enter your first name",
-              pointing: "below"
-            }}
+            error={errorFirstName}
             fluid
             required
             placeholder="First Name"
@@ -92,10 +162,7 @@ function Registration() {
             onChange={handleChange}
           />
           <Form.Input
-            error={{
-              content: "Please enter your last name",
-              pointing: "below"
-            }}
+            error={errorLastName}
             fluid
             required
             placeholder="Last Name"
@@ -158,10 +225,7 @@ function Registration() {
           </Form.Group>
 
           <Form.Input
-            error={{
-              content: "Please enter your email",
-              pointing: "below"
-            }}
+            error={errorEmail}
             fluid
             required
             placeholder="Email"
@@ -169,12 +233,21 @@ function Registration() {
             onChange={handleChange}
           />
 
-          <Form.Button>Register</Form.Button>
+          <Form.Button onClick={handleSubmit}>Register</Form.Button>
         </Form>
         <strong>onChange:</strong>
         <pre>
           {JSON.stringify(
-            { phoneNumber, firstName, lastName, gender, month, date, year, email },
+            {
+              phoneNumber,
+              firstName,
+              lastName,
+              gender,
+              month,
+              date,
+              year,
+              email
+            },
             null,
             8
           )}
